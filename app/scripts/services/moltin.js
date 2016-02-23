3ab28e16c991a964f5ee9acd54cf19aa10f2deae
SlootSantos <A605614@defm.awl.atosorigin.net>
2016-02-23 08:49:28 +0100

    'add'

--- app/scripts/services/moltin.js
+++ app/scripts/services/moltin.js
@@ -1,7 +1,7 @@
 angular.module('webshopApp.moltin', [])
   .factory('MoltinAuth', function($q) {
     var deffered = $q.defer();
-    var moltin = new Moltin({publicId: ''});
+    var moltin = new Moltin({publicId: 'dwfun8ekseCvZ9hERZrxNOfdyjYhBTfRaiyXSNcM'});
     moltin.Authenticate(function() {
       deffered.resolve(moltin);
     });
