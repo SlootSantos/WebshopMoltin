e9c33d75718d1b681890e734295c0f2bec86458e
SlootSantos <A605614@defm.awl.atosorigin.net>
2016-02-23 11:47:01 +0100

    'cartview'

--- app/scripts/controllers/cart.js
+++ app/scripts/controllers/cart.js
@@ -8,15 +8,25 @@
  * Controller of the webshopMoltinApp
  */
 angular.module('webshopMoltinApp')
-  .controller('CartCtrl', function ($scope, cart, moltin) {
+  .controller('CartCtrl', function ($scope, $rootScope, cart, moltin) {
     $scope.cartDisplay = cart;
     console.log(cart.contents);
     console.log(cart.total_items);
+    console.log(cart.totals.post_discount.formatted.with_tax);
+
     $scope.delete = function () {
           moltin.Cart.Delete(function() {
             // Everything is awesome...
+            $scope.$apply(function(){
+              $scope.cartDisplay = cart;
+            });
+            console.log(cart.totals.post_discount.formatted.with_tax);
+            moltin.Cart.Contents(function(items) {
+              $rootScope.cart = items;
+              $rootScope.$apply();
+            });
         }, function() {
             // Something went wrong...
         });
-    };
+      };
   });
