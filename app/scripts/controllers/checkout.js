05f667de6779c91f88af4cb2543cecb534a4469c
SlootSantos <A605614@defm.awl.atosorigin.net>
2016-02-23 16:14:21 +0100

    'checkout_aint_workin_yet'

--- app/scripts/controllers/checkout.js
+++ app/scripts/controllers/checkout.js
@@ -8,10 +8,25 @@
  * Controller of the webshopMoltinApp
  */
 angular.module('webshopMoltinApp')
-  .controller('CheckoutCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+.controller('CheckoutCtrl', function ($scope, $rootScope, $location, moltin, cart, options, fields) {
+    $scope.data = {bill: {}, ship: {}, ship_bill: 0, notes: '', shipping: '', gateway: ''}
+    $scope.cartDisplay = cart;
+    $scope.options = options;
+    $scope.fields = fields;
+
+    $scope.createOrder = function() {
+      moltin.Order.Create({
+        customer: '1191351375552839932', //guest customer for now
+        shipping: $scope.data.shipping,
+      /*  gateway: $scope.data.gateway,*/
+        bill_to: $scope.data.bill,
+        ship_to: $scope.data.ship_bill ? 'bill_to' : $scope.data.ship
+      }, function(response) {
+        $rootScope.order = response;
+        $rootScope.$apply(function() {
+          $location.path('/payment');
+        });
+      })
+    }
+
   });
--- app/scripts/controllers/payment.js
+++ app/scripts/controllers/payment.js
@@ -1,17 +1,22 @@
+
 'use strict';
 
 /**
  * @ngdoc function
- * @name webshopMoltinApp.controller:PaymentCtrl
+ * @name storefrontApp.controller:PaymentCtrl
  * @description
  * # PaymentCtrl
- * Controller of the webshopMoltinApp
+ * Controller of the storefrontApp
  */
 angular.module('webshopMoltinApp')
-  .controller('PaymentCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('PaymentCtrl', function ($scope, $location, $rootScope, moltin) {
+    $scope.payment = function(data) {
+      moltin.Checkout.Payment('purchase', $scope.order.id, {data: $scope.data}, function(response) {
+        $rootScope.order = $rootScope.cart = null;
+        $rootScope.payment = response;
+        $rootScope.$apply(function() {
+          $location.path('/complete');
+        });
+      });
+    }
   });
